be5ec837c16d31dd575d5e0f83033256a897ef3a
Byteme <shvartsmand@gmail.com>
2018-07-25 23:37:40 -0700

    sequelize start

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,25 @@
 const router = require("express").Router();
-const Users = require("../models/users.js");
-const QRcodes = require("../models/qrcode.js");
-const Proudcts = require("../models/products.js");
+// const Users = require("../models/Users.js");
+// const QRcodes = require("../models/Qrcode.js");
+// const Bills = require("../models/Bills.js");
+const db = require("../models");
+
+
+router.post("/api/bills", function (req, res) {
+    console.log(req.body);
+    db.Bills.create({
+            items: req.body.items,
+            price: req.body.price,
+            qty: req.body.qty
+        })
+        .then(function (dbBills) {
+            res.json(dbBills);
+        });
+});
+
+
+
+
 
 
 module.exports = router;
