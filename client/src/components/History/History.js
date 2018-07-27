0c35c569544314a40015a1078f16ba40951b47d9
Lucas Ahn <Lucas@Lucass-MacBook.local>
2018-07-27 10:47:50 -0700

    basic UI

---
+++ client/src/components/History/History.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const History = () => {
+  return (
+    <header>
+      History Page
+    </header>
+  );
+};
+
+export default History;
---
+++ client/src/components/QR/QR.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class QR extends React.Component {
+    render() {
+        return (
+            <div>QR PAGE!</div>
+        )
+    }
+}
+
+export default QR;
