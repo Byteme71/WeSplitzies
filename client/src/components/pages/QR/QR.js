604752090106c444e87af04681af6f560dc21bc7
Byteme <shvartsmand@gmail.com>
2018-07-30 01:02:05 -0700

    fixed-after-merge-qr-code-up

--- client/src/components/pages/QR/QR.js
+++ client/src/components/pages/QR/QR.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 class QR extends React.Component {
+    toBillPage = () => {
+        window.location.href = "/bill";
+    }
+    
     render() {
         return (
-            <div>QR PAGE!</div>
+        <div>
+    
+                <img onClick={this.toBillPage}src="https://www.qrstuff.com/images/sample.png" height="200px" width="200px"/>        
+
+        </div>
         )
     }
 }
