56a10471146cb3fdf7457b2579603584f08542b3
Byteme <shvartsmand@gmail.com>
2018-07-31 14:43:27 -0700

    something

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,30 @@
-// import React from "react";
+import React from "react";
 
-// import Navbar from "./components/Navbar.js";
-// const App = () => <Navbar />;
+import Navbar from "./components/Navbar.js";
+const App = () => <Navbar />;
 
 // // const App = () => <Home />;
 
-// export default App;
-
-
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Switch } from 'react-router'
-import Home from "./components/Home.js";
-import Auth from "./components/Auth.js";
-
-class App extends React.Component {  
-    render() {
-        return(
-            <Router>
-                <Switch>
-                    <Route path="/" component={Home} />
-                    <Auth />
-                </Switch>
-            </Router>
-        )
-    }
-}
-
 export default App;
+
+
+// import React from "react";
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+// import { Switch } from 'react-router'
+// import Home from "./components/Home.js";
+// import Auth from "./components/Auth.js";
+
+// class App extends React.Component {  
+//     render() {
+//         return(
+//             <Router>
+//                 <Switch>
+//                     <Route path="/" component={Home} />
+//                     <Auth />
+//                 </Switch>
+//             </Router>
+//         )
+//     }
+// }
+
+// export default App;
